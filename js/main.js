8fd59e50051b4633657a84025965c679d967fbf9
Suleman-Zakaria <suleman.zakaria@amalitech.org>
2020-10-09 12:04:34 +0000

    selecting inputs

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@
   //console.log($(document));
   $('body').text("Hello word");
   $('body').html('<strong>I am good to go.</strong>')
-});*/
+});
 $(document).ready(function(){
     $('.solid').addClass('highlight-solid');
 });
 $(document).ready(function(){
     $('.non-solid').addClass('highlight-non-solid');
+});*/
+$(document).ready(function(){
+    /*Selection of the id: container, name ; input with feature: required*/
+    $('#container input:required').addClass('highlight')
 });
