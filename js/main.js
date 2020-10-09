6933fb9843aa536146cae7c45639c1e00983ae8b
Suleman-Zakaria <suleman.zakaria@amalitech.org>
2020-10-09 11:12:26 +0000

    adding a list to the index.html

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//console.log("Hello world");
+/*$(document).ready(function(){
+  console.log("Are you ready?"); 
+  //console.log($(document));
+  $('body').text("Hello word");
+  $('body').html('<strong>I am good to go.</strong>')
+});*/
